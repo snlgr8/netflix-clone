056ae66f69a8ff817f549fba012a531f52f687b6
sonal.chaplot <sonal.chaplot@dieboldnixdorf.com>
2020-10-13 22:38:58 +0530

    Updates

---
+++ precache-manifest.62b2f32a67eb31bf0ca7e05afb83b244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1ee4b2317c7fc35bd2ff0f190b77ac",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "cc0bf772652485aac19e",
+    "url": "/netflix-clone/static/css/main.38fe2582.chunk.css"
+  },
+  {
+    "revision": "9f813589bceb27e8cd37",
+    "url": "/netflix-clone/static/js/2.32b80272.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.32b80272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0bf772652485aac19e",
+    "url": "/netflix-clone/static/js/main.4409a4a4.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
